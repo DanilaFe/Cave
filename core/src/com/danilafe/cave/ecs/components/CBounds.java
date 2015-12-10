0ab5de09805d4ba94f00b04b922f6c6eae560e38
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-09 22:10:44 -0800

    JavaDoc Ayy

--- core/src/com/danilafe/cave/ecs/components/CBounds.java
+++ core/src/com/danilafe/cave/ecs/components/CBounds.java
@@ -4,9 +4,17 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
+/**
+ * Bounds component. Holds a rectangle that is updated with the position and the size of this entity.
+ * @author vanilla
+ *
+ */
 public class CBounds implements Poolable, Component {
 
-	private Rectangle bounds = new Rectangle(0, 0, 0, 0);
+	/**
+	 * The rectangle that defines the bounds of this entity.
+	 */
+	public Rectangle bounds = new Rectangle(0, 0, 0, 0);
 	
 	@Override
 	public void reset() {
--- core/src/com/danilafe/cave/ecs/components/CPosition.java
+++ core/src/com/danilafe/cave/ecs/components/CPosition.java
@@ -4,8 +4,16 @@ import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
+/**
+ * Position component. Holds the position of the entity
+ * @author vanilla
+ *
+ */
 public class CPosition implements Poolable, Component {
 
+	/**
+	 * Vector2 holding the x and y coordinates in the game world.
+	 */
 	public Vector2 position = new Vector2(0, 0);
 	
 	@Override
