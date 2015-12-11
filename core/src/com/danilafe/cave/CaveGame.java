7135d3f3a8846eac6156150c39b103d9d323e0bd
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-10 21:19:18 -0800

    Systems are in here now

--- core/src/com/danilafe/cave/CaveGame.java
+++ core/src/com/danilafe/cave/CaveGame.java
@@ -1,12 +1,25 @@
 package com.danilafe.cave;
 
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.danilafe.cave.animation.Animation;
+import com.danilafe.cave.animation.AnimationParameter;
+import com.danilafe.cave.ecs.components.CAnimation;
+import com.danilafe.cave.ecs.components.CGravity;
+import com.danilafe.cave.ecs.components.CPosition;
+import com.danilafe.cave.ecs.components.CSpeed;
+import com.danilafe.cave.ecs.systems.BoundsSystem;
+import com.danilafe.cave.ecs.systems.DebugRenderSystem;
+import com.danilafe.cave.ecs.systems.GravitySystem;
+import com.danilafe.cave.ecs.systems.PositionSystem;
 import com.danilafe.cave.ecs.systems.RenderSystem;
 
 /**
@@ -26,6 +39,10 @@ public class CaveGame extends ApplicationAdapter {
 	 * RenderSystem - renders stuff
 	 */
 	public RenderSystem renderSystem;
+	public BoundsSystem boundsSystem;
+	public DebugRenderSystem debugRenderSystem;
+	public GravitySystem gravitySystem;
+	public PositionSystem positionSystem;
 	public OrthographicCamera orthoCam;
 	
 	
@@ -37,9 +54,18 @@ public class CaveGame extends ApplicationAdapter {
 		instance = this;
 		pooledEngine = new PooledEngine(100, 100, 300, 300);
 		renderSystem = new RenderSystem();
+		boundsSystem = new BoundsSystem();
+		debugRenderSystem = new DebugRenderSystem();
+		gravitySystem = new GravitySystem();
+		positionSystem = new PositionSystem();
+		
 		orthoCam = new OrthographicCamera(Constants.CAMERA_WIDTH, Constants.CAMERA_WIDTH * Gdx.graphics.getHeight() / Gdx.graphics.getWidth());
 		
 		pooledEngine.addSystem(renderSystem);
+		pooledEngine.addSystem(boundsSystem);
+		pooledEngine.addSystem(debugRenderSystem);
+		pooledEngine.addSystem(gravitySystem);
+		pooledEngine.addSystem(positionSystem);
 	}
 
 	@Override
