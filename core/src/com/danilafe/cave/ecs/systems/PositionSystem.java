a8ffc961234c84970335b212f1029fb0cdfec74a
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-10 21:18:32 -0800

    Remove modifier - they will not be used.

--- core/src/com/danilafe/cave/ecs/systems/PositionSystem.java
+++ core/src/com/danilafe/cave/ecs/systems/PositionSystem.java
@@ -3,27 +3,24 @@ package com.danilafe.cave.ecs.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.danilafe.cave.ecs.components.CModifiable;
 import com.danilafe.cave.ecs.components.CPosition;
 import com.danilafe.cave.ecs.components.CSpeed;
 
 public class PositionSystem extends IteratingSystem {
 
 	public PositionSystem() {
-		super(Family.all(CPosition.class).one(CSpeed.class, CModifiable.class).get());
+		super(Family.all(CPosition.class).all(CSpeed.class).get());
 	}
 
 	@Override
 	protected void processEntity(Entity entity, float deltaTime) {
 		CSpeed entitySpeed = entity.getComponent(CSpeed.class);
-		CModifiable entityModifiable = entity.getComponent(CModifiable.class);
 		CPosition entityPosition = entity.getComponent(CPosition.class);
 		
 		/*
-		 * Add the things to position.
+		 * Add the speed to position.
 		 */
 		if (entitySpeed != null)  entityPosition.position.add(entitySpeed.speed.cpy().scl(deltaTime));
-		if (entityModifiable != null) entityPosition.position.add(entityModifiable.modifierContainer.modifierSum.cpy().scl(deltaTime));
 	}
 
 }
