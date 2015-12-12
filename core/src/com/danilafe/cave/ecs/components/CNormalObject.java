3f12be2d7a39529e8e1d0f3da44dd29094cd1ccf
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-11 19:33:38 -0800

    Implement the necessary things.

--- core/src/com/danilafe/cave/ecs/components/CNormalObject.java
+++ core/src/com/danilafe/cave/ecs/components/CNormalObject.java
@@ -1,11 +1,19 @@
 package com.danilafe.cave.ecs.components;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool.Poolable;
+
 /**
  * This component is used to flag an entity as something that will be pushed out of 
  *  Normal Obstacles
  * @author vanilla
  *
  */
-public class CNormalObject {
+public class CNormalObject implements Poolable, Component {
+
+	@Override
+	public void reset() {
+		
+	}
 
 }
