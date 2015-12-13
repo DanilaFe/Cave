63b9af720281b84e5b1d174fb4987e3ab5500a72
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-12 16:57:54 -0800

    Shader; also fix 1GB/sec memory leak. How did I even not notice this?

--- core/src/com/danilafe/cave/ecs/systems/RenderSystem.java
+++ core/src/com/danilafe/cave/ecs/systems/RenderSystem.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.danilafe.cave.CaveGame;
 import com.danilafe.cave.Constants;
 import com.danilafe.cave.animation.Animation;
@@ -19,12 +20,16 @@ public class RenderSystem extends IteratingSystem {
 	
 	SpriteBatch mainBatch;
 	SpriteBatch bufferBatch;
+	ShaderProgram shaderProgram;
 	FrameBuffer mainBuffer;
 	
 	public RenderSystem() {
 		super(Family.all(CAnimation.class, CPosition.class).get());
 		mainBatch = new SpriteBatch();
 		bufferBatch = new SpriteBatch();
+		shaderProgram = CaveGame.loadShaders("greyscale");
+		
+		bufferBatch.setShader(shaderProgram);
 	}
 
 	@Override
@@ -44,7 +49,7 @@ public class RenderSystem extends IteratingSystem {
 		mainBuffer = new FrameBuffer(Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
 		mainBuffer.begin();
 		Gdx.gl.glViewport(0, 0, mainBuffer.getWidth(), mainBuffer.getHeight());
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT | Gdx.gl.GL_DEPTH_BUFFER_BIT);
 		
 		mainBatch.setProjectionMatrix(CaveGame.instance.orthoCam.combined);
@@ -61,6 +66,7 @@ public class RenderSystem extends IteratingSystem {
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
 		bufferBatch.draw(mainBuffer.getColorBufferTexture(), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0, 0, 1, 1);
 		bufferBatch.end();
+		mainBuffer.dispose();
 	}
 
 }
