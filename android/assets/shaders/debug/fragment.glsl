e3044e803d511fb0ad7a87b0677b23a01b23fd65
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-13 21:06:52 -0800

    Testing light rendering code. This isn't final.

--- android/assets/shaders/debug/fragment.glsl
+++ android/assets/shaders/debug/fragment.glsl
@@ -7,7 +7,19 @@ varying vec2 v_texCoords;
 uniform sampler2D u_texture;
 uniform int u_textureWidth;
 uniform int u_textureHeight;
+uniform int u_texOffsetX;
+uniform int u_texOffsety;
+uniform int u_numLights;
+uniform vec3 u_lightColors[128];
+uniform vec3 u_lightProps[128];
 
 void main() {
-	gl_FragColor = (v_color * texture2D(u_texture, v_texCoords));
+	vec3 totalLights = vec3(0.0, 0.0, 0.0);
+	vec2 worldPos = vec2(floor(v_texCoords.x * float(u_textureWidth)) + float(u_texOffsetX), floor(v_texCoords.y * float(u_textureHeight)) + float(u_texOffsety));
+	for(int i = 0; i < u_numLights; i++){
+		float light_percent = u_lightProps[i].z / (distance(u_lightProps[i].xy, worldPos) + 1.0);
+		totalLights = totalLights + (u_lightColors[i] * light_percent);
+	}
+	normalize(totalLights);
+	gl_FragColor = (v_color * texture2D(u_texture, v_texCoords)) * vec4(totalLights, 1);
 }
--- core/src/com/danilafe/cave/ecs/systems/RenderSystem.java
+++ core/src/com/danilafe/cave/ecs/systems/RenderSystem.java
@@ -10,11 +10,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector2;
 import com.danilafe.cave.CaveGame;
 import com.danilafe.cave.Constants;
 import com.danilafe.cave.animation.Animation;
 import com.danilafe.cave.ecs.components.CAnimation;
 import com.danilafe.cave.ecs.components.CPosition;
+import com.danilafe.cave.lights.Light;
+import com.danilafe.cave.lights.LightManager;
 
 /**
  * RenderSystem - renders the game.
@@ -39,12 +42,15 @@ public class RenderSystem extends IteratingSystem {
 	 * The framebuffer used to render everything before shader is applied.
 	 */
 	FrameBuffer mainBuffer;
+	LightManager lightManager;
 	
 	public RenderSystem() {
 		super(Family.all(CAnimation.class, CPosition.class).get());
 		mainBatch = new SpriteBatch();
 		bufferBatch = new SpriteBatch();
 		shaderProgram = CaveGame.loadShaders("debug");
+		lightManager = new LightManager();
+		lightManager.lights.add(Light.create(80, 20, 10, .4F, 1F, 1F));
 		
 		bufferBatch.setShader(shaderProgram);
 	}
@@ -66,7 +72,7 @@ public class RenderSystem extends IteratingSystem {
 		mainBuffer = new FrameBuffer(Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
 		mainBuffer.begin();
 		Gdx.gl.glViewport(0, 0, mainBuffer.getWidth(), mainBuffer.getHeight());
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(.5F, .5F, .5F, 1);
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT | Gdx.gl.GL_DEPTH_BUFFER_BIT);
 		
 		mainBatch.setProjectionMatrix(CaveGame.instance.orthoCam.combined);
@@ -79,8 +85,17 @@ public class RenderSystem extends IteratingSystem {
 		
 		bufferBatch.getProjectionMatrix().setToOrtho2D(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		bufferBatch.begin();
-		shaderProgram.setUniformi("u_textureWidth", (int) CaveGame.instance.orthoCam.viewportWidth * 2);
-		shaderProgram.setUniformi("u_textureHeight", (int) CaveGame.instance.orthoCam.viewportHeight * 2);
+		shaderProgram.setUniformi("u_textureWidth", (int) CaveGame.instance.orthoCam.viewportWidth);
+		shaderProgram.setUniformi("u_textureHeight", (int) CaveGame.instance.orthoCam.viewportHeight);
+		shaderProgram.setUniformi("u_texOffsetX", (int) (CaveGame.instance.orthoCam.position.x - CaveGame.instance.orthoCam.viewportWidth / 2));
+		shaderProgram.setUniformi("u_texOffsetY", (int) (CaveGame.instance.orthoCam.position.y - CaveGame.instance.orthoCam.viewportHeight / 2));
+		Gdx.app.log("Lights", "Rendering " + lightManager.lights.size() + " lights");
+		lightManager.sortByDistance(new Vector2(CaveGame.instance.orthoCam.position.x, CaveGame.instance.orthoCam.position.y));
+		shaderProgram.setUniformi("u_numLights", (lightManager.maxLights < lightManager.lights.size()) ? lightManager.maxLights : lightManager.lights.size());
+		for(int i = 0; i < lightManager.lights.size() && i < lightManager.maxLights; i++){
+			shaderProgram.setUniform3fv("u_lightColors[" + i + "]", lightManager.lights.get(i).rgb , 0, 3);
+			shaderProgram.setUniform3fv("u_lightProps[" + i + "]", lightManager.lights.get(i).getPropertyArray(), 0, 3);
+		}
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
 		bufferBatch.draw(mainBuffer.getColorBufferTexture(), 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 0, 0, 1, 1);
