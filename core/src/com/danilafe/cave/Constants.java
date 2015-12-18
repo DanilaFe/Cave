7560f3c11215cb7228bfb18ea828e22a5376d7bb
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-18 09:30:59 -0800

    Enable debug mode

--- core/src/com/danilafe/cave/Constants.java
+++ core/src/com/danilafe/cave/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	 * The width of the in-game world view. Height scales with window size.
 	 */
 	public static final float CAMERA_WIDTH = 100;
-	public static final boolean DEBUG = false;
+	public static final boolean DEBUG = true;
 	public static final boolean NORMAL = false;
 	
 }
