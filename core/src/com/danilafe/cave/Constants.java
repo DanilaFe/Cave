5a5e22eb35cac0b990e14596063be62197068dbb
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-22 13:48:01 -0800

    Now that we have separate variables to control debug output, enable global debugging.

--- core/src/com/danilafe/cave/Constants.java
+++ core/src/com/danilafe/cave/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 	/**
 	 * Whether the game should start in debug mode or not.
 	 */
-	public static final boolean DEBUG = false;
+	public static final boolean DEBUG = true;
 	/**
 	 * Whether normal maps should be used.
 	 */
