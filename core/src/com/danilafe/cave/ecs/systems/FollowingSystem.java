11bad7b8237dfeb6696584b3946deaa257055640
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-23 19:17:59 -0800

    Tweak following system to update nested following entities.

--- core/src/com/danilafe/cave/ecs/systems/FollowingSystem.java
+++ core/src/com/danilafe/cave/ecs/systems/FollowingSystem.java
@@ -3,6 +3,8 @@ package com.danilafe.cave.ecs.systems;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.utils.Array;
 import com.danilafe.cave.ecs.components.CFollow;
 import com.danilafe.cave.ecs.components.CPosition;
 
@@ -21,11 +23,27 @@ public class FollowingSystem extends IteratingSystem {
 	}
 
 	@Override
-	protected void processEntity(Entity entity, float deltaTime) {
-		CFollow follow = entity.getComponent(CFollow.class);
-		CPosition position = entity.getComponent(CPosition.class);
-		CPosition otherPos = follow.following.getComponent(CPosition.class);
-		if(otherPos != null) position.position.set(otherPos.position.cpy().add(follow.offset));
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+		ImmutableArray<Entity> entities = getEntities();
+		Array<Entity> toProcess = new Array<Entity>();
+		for(int i = 0; i < entities.size(); i++){
+			toProcess.add(entities.get(i));
+		}
+
+		while(toProcess.size > 0){
+			recursiveFollow(toProcess.get(0), toProcess);
+		}
 	}
+
+	public void recursiveFollow(Entity e, Array<Entity> removeFrom){
+		CFollow follow = e.getComponent(CFollow.class);
+		if(getFamily().matches(follow.following)) recursiveFollow(follow.following, removeFrom);
+		e.getComponent(CPosition.class).position.set(follow.following.getComponent(CPosition.class).position.cpy().add(follow.offset));
+		removeFrom.removeValue(e, false);
+	}
+
+	@Override
+	protected void processEntity(Entity entity, float deltaTime) { }
 
 }
