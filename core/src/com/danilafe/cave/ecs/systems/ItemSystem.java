c38dbf996fa3f9319e298a7b0297abd891ff5df6
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-26 13:05:30 -0800

    Write an item system to check for overflows. That's all it does so far.

---
+++ core/src/com/danilafe/cave/ecs/systems/ItemSystem.java
@@ -0,0 +1,24 @@
+package com.danilafe.cave.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.danilafe.cave.ecs.components.CItem;
+import com.danilafe.cave.ecs.components.CItemContainer;
+
+public class ItemSystem extends FamilySystem {
+
+	public ItemSystem() {
+		super(Family.all(CItem.class).get(), Family.all(CItemContainer.class).get());
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+
+		for(Entity container : entitiesB){
+			CItemContainer itemContainer = container.getComponent(CItemContainer.class);
+			if(itemContainer.container.items.size() > itemContainer.container.maxItems) itemContainer.container.onOverflow.update(container, deltaTime);
+		}
+	}
+
+}
