a83756f57daa10d9bf561e8908abb8cad57df48e
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-27 13:36:18 -0800

    A tile component.

---
+++ core/src/com/danilafe/cave/ecs/components/CTile.java
@@ -0,0 +1,18 @@
+package com.danilafe.cave.ecs.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.utils.Pool.Poolable;
+
+/**
+ * Tile component. Makes this entity a tile.
+ * @author vanilla
+ *
+ */
+public class CTile implements Poolable, Component {
+
+	@Override
+	public void reset() {
+
+	}
+
+}
