4ff971a9c2e4525f5ce3ffae386e82f15cde7236
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-27 13:34:36 -0800

    TileAnimation houses the different animations a tile can have in different positions.

---
+++ core/src/com/danilafe/cave/tile/TileAnimation.java
@@ -0,0 +1,40 @@
+package com.danilafe.cave.tile;
+
+import com.danilafe.cave.animation.Animation;
+
+/**
+ * The animation set of a tile.
+ * @author vanilla
+ *
+ */
+public class TileAnimation {
+
+	/**
+	 * Animation when this tile has one neighbor
+	 */
+	public Animation oneNeighbor;
+	/**
+	 * Animation when this tile has two adjacent neighbors
+	 */
+	public Animation corner;
+	/**
+	 * Animation when this tile has no exposed sides.
+	 */
+	public Animation fourNeighbor;
+	/**
+	 * Creates a new TileAnimation from the given parameters
+	 * @param oneNeighbor the animation when this tile has one neighbor
+	 * @param corner the animation when this tile has two adjacent neighbors
+	 * @param fourNeighbor the animation when this tile has no exposed sides.
+	 * @return
+	 */
+	public static TileAnimation create(Animation oneNeighbor, Animation corner, Animation fourNeighbor){
+		TileAnimation newAnimation = new TileAnimation();
+		newAnimation.oneNeighbor = oneNeighbor;
+		newAnimation.corner = corner;
+		newAnimation.fourNeighbor = fourNeighbor;
+
+		return newAnimation;
+	}
+
+}
