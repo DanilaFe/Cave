8a8d00f50479659b281bbfa121393b5f1b4766f5
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-28 00:29:43 -0800

    Add more potential variations

--- core/src/com/danilafe/cave/tile/TileAnimation.java
+++ core/src/com/danilafe/cave/tile/TileAnimation.java
@@ -12,11 +12,19 @@ public class TileAnimation {
 	/**
 	 * Animation when this tile has one neighbor
 	 */
-	public Animation oneNeighbor[];
+	public Animation[] oneNeighbor;
 	/**
 	 * Animation when this tile has two adjacent neighbors
 	 */
-	public Animation corner[];
+	public Animation[] corner;
+	/**
+	 * Animation when this tile has two non-adjacent neighbors
+	 */
+	public Animation[] twoNeighbors;
+	/**
+	 * Animation when this tile has three neighbors.
+	 */
+	public Animation[] threeNeighbors;
 	/**
 	 * Animation when this tile has no exposed sides.
 	 */
@@ -26,13 +34,17 @@ public class TileAnimation {
 	 * @param oneNeighbor the animation when this tile has one neighbor
 	 * @param corner the animation when this tile has two adjacent neighbors
 	 * @param fourNeighbor the animation when this tile has no exposed sides.
-	 * @return
+	 * @param twoNeighbors the animation when this tile has two non-adjacent neighbors
+	 * @param threeNeighbors the animation when this tile has three neighbors
+	 * @return the created TileAnimation
 	 */
-	public static TileAnimation create(Animation[] oneNeighbor, Animation[] corner, Animation[] fourNeighbor){
+	public static TileAnimation create(Animation[] oneNeighbor, Animation[] corner, Animation[] fourNeighbor, Animation[] twoNeighbors, Animation[] threeNeighbors){
 		TileAnimation newAnimation = new TileAnimation();
 		newAnimation.oneNeighbor = oneNeighbor;
 		newAnimation.corner = corner;
 		newAnimation.fourNeighbor = fourNeighbor;
+		newAnimation.twoNeighbors = twoNeighbors;
+		newAnimation.threeNeighbors = threeNeighbors;
 
 		return newAnimation;
 	}
