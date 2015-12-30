6a34d42fd747877ba468200702a7bd387a422f48
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-29 22:56:24 -0800

    Damage data to hold data about damage. Duh.

---
+++ core/src/com/danilafe/cave/health/DamageData.java
@@ -0,0 +1,11 @@
+package com.danilafe.cave.health;
+
+import com.badlogic.ashley.core.Entity;
+
+public class DamageData {
+
+	public Entity damageTo;
+	public Entity damageFrom;
+	public float damage;
+
+}
