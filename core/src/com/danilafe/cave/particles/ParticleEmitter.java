87a558235a0baa77ca8e310fce50e1d50d46dda5
Danila Fedorin <danila.fedorin@gmail.com>
2015-12-29 18:13:17 -0800

    Write a particle emitter

---
+++ core/src/com/danilafe/cave/particles/ParticleEmitter.java
@@ -0,0 +1,86 @@
+package com.danilafe.cave.particles;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.math.Vector2;
+import com.danilafe.cave.CaveGame;
+import com.danilafe.cave.creation.EntityDescriptor;
+import com.danilafe.cave.ecs.components.CMarked;
+
+/**
+ * A particle emitter spawning particles.
+ * @author vanilla
+ *
+ */
+public class ParticleEmitter {
+
+	/**
+	 * The position of this emitter
+	 */
+	public Vector2 position = new Vector2();
+	/**
+	 * The minimum delay (in seconds) before a new particle is spawned
+	 */
+	public float minDelay;
+	/**
+	 * How much longer than the minimum delay it's possible to wait
+	 */
+	public float maxDelayFluctuation;
+	/**
+	 * The minimum range of the emitter
+	 */
+	public float minRange;
+	/**
+	 * The maximum distance beyond the minimum range this emitter can go
+	 */
+	public float maxRangeFluctuation;
+	/**
+	 * The minimum direction, in degrees, in which particles are spawned from the emitter
+	 */
+	public float minDirection;
+	/**
+	 * The maximum additional angle beyond the minimum angle at which particles can be spawned
+	 */
+	public float maxDirectionFluctuation;
+	/**
+	 * Delay remaining before next particle is spawned.
+	 */
+	public float delay;
+	/**
+	 * The type of entity that's spawned as a particle
+	 */
+	public String entityType;
+	/**
+	 * If true, and the spawned entity has a CMarked component, the emitter will add a "particle" marker to the component.
+	 */
+	public boolean markAsParticle;
+	/**
+	 * The descriptor of the entity type. Used for caching to prevent requesting the entity descriptor multiple times.
+	 */
+	private EntityDescriptor retreivedDescriptor;
+
+	/**
+	 * Updates this emitter.
+	 * @param deltaTime
+	 */
+	public void update(float deltaTime) {
+		delay -= deltaTime;
+		while(delay <= 0){
+			if(retreivedDescriptor == null) retreivedDescriptor = CaveGame.instance.creationManager.entityDescriptors.get(entityType);
+			float addedDelay = (float) (Math.random() * maxDelayFluctuation + minDelay);
+			float direction = (float) (Math.random() * maxDirectionFluctuation + minDirection);
+			float distance = (float) (Math.random() * maxRangeFluctuation + minRange);
+
+			Vector2 offset = new Vector2(1, 0);
+			offset.setAngle(direction);
+			offset.setLength(distance);
+			offset.add(position);
+
+			Entity newParticle = retreivedDescriptor.create(offset.x, offset.y);
+			if(markAsParticle && Family.all(CMarked.class).get().matches(newParticle)) newParticle.getComponent(CMarked.class).marks.put("particle", true);
+			CaveGame.instance.pooledEngine.addEntity(newParticle);
+			delay += addedDelay;
+		}
+	}
+
+}
