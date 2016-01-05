8852e3995ec6307062d5f8afb073fa16561c8f2d
Danila Fedorin <danila.fedorin@gmail.com>
2016-01-04 21:45:05 -0800

    SpeedDamage component

---
+++ core/src/com/danilafe/cave/ecs/components/CSpeedDamage.java
@@ -0,0 +1,13 @@
+package com.danilafe.cave.ecs.components;
+
+/**
+ * The SpeedDamage component deals damage to the entity if its speed changes too fast.
+ * @author vanilla
+ *
+ */
+public class CSpeedDamage {
+
+	public float maxSpeed = Float.MAX_VALUE;
+	public float damagePerUnit = 0;
+
+}
