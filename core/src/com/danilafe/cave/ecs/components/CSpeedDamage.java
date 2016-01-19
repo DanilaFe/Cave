cdc366757c990ec833796d91ee74af76249b71dd
Danila Fedorin <danila.fedorin@gmail.com>
2016-01-18 18:00:48 -0800

    Make the damage component a component.

--- core/src/com/danilafe/cave/ecs/components/CSpeedDamage.java
+++ core/src/com/danilafe/cave/ecs/components/CSpeedDamage.java
@@ -1,13 +1,25 @@
 package com.danilafe.cave.ecs.components;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Pool.Poolable;
+
 /**
  * The SpeedDamage component deals damage to the entity if its speed changes too fast.
  * @author vanilla
  *
  */
-public class CSpeedDamage {
+public class CSpeedDamage implements Poolable, Component {
 
+	public Vector2 previousSpeed = new Vector2(Float.MAX_VALUE, Float.MAX_VALUE);
 	public float maxSpeed = Float.MAX_VALUE;
 	public float damagePerUnit = 0;
+
+	@Override
+	public void reset() {
+		damagePerUnit = 0;
+		maxSpeed = Float.MAX_VALUE;
+		previousSpeed.set(Float.MAX_VALUE, Float.MAX_VALUE);
+	}
 
 }
