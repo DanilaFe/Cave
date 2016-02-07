17b5704abb94edeb70bdedb2630dc7a05dcdbee5
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-06 22:23:33 -0800

    Create a weapon descriptor class, to re-use weapon data.

---
+++ core/src/com/danilafe/cave/attacks/WeaponDecriptor.java
@@ -0,0 +1,11 @@
+package com.danilafe.cave.attacks;
+
+import com.danilafe.cave.creation.EntityDescriptor;
+
+public class WeaponDecriptor {
+
+	public WeaponParameter weaponParameter = null;
+	public WeaponPropertiesCalculator weaponType = null;
+	public EntityDescriptor entityDescriptor = null;
+
+}
