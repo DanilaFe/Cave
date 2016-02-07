ad73e92f56e7b8bfdb822f07fdb0e15d74c37273
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-06 18:37:05 -0800

    Update to LibGDX 1.9.1

--- desktop/src/com/danilafe/cave/desktop/DesktopLauncher.java
+++ desktop/src/com/danilafe/cave/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package com.danilafe.cave.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.danilafe.cave.CaveGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CaveGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new CaveGame(), config);
 	}
 }
