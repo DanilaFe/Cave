56c130b2cda20961838483ff30bb2d063a623c08
Danila Fedorin <danila.fedorin@gmail.com>
2016-02-06 22:24:40 -0800

    Remove following in favor of CFollow

--- core/src/com/danilafe/cave/ecs/components/CWeapon.java
+++ core/src/com/danilafe/cave/ecs/components/CWeapon.java
@@ -1,19 +1,16 @@
 package com.danilafe.cave.ecs.components;
 
 import com.badlogic.ashley.core.Component;
-import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.utils.Pool.Poolable;
 import com.danilafe.cave.attacks.Weapon;
 
 public class CWeapon implements Poolable, Component {
 
 	public Weapon weapon = null;
-	public Entity following = null;
 
 	@Override
 	public void reset() {
 		weapon = null;
-		following = null;
 	}
 
 }
