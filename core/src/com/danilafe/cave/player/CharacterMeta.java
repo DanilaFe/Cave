b450da778d909a803a56208b1c000352c1cbfe36
Danila Fedorin <danila.fedorin@gmail.com>
2016-03-09 20:36:06 -0800

    Boilerplate character code

---
+++ core/src/com/danilafe/cave/player/CharacterMeta.java
@@ -0,0 +1,11 @@
+package com.danilafe.cave.player;
+
+/**
+ * Placeholder class for when character appearence values will be stored.
+ * @author vanilla
+ *
+ */
+public class CharacterMeta {
+
+
+}
---
+++ core/src/com/danilafe/cave/player/PlayerStatistic.java
@@ -0,0 +1,36 @@
+package com.danilafe.cave.player;
+
+/**
+ * Class describing a player.
+ * @author vanilla
+ *
+ */
+public class PlayerStatistic {
+
+	/**
+	 * Number of games won by the player
+	 */
+	public int gameWins;
+	/**
+	 * Number of games lost by the player
+	 */
+	public int gameLoss;
+	/**
+	 * Total games played
+	 */
+	public int totalGames;
+	/**
+	 * Name of the player
+	 */
+	public String name;
+	/**
+	 * The character meta data of this player
+	 */
+	public CharacterMeta characterMeta;
+	/**
+	 * The key binds of this player
+	 */
+	public KeyBinds keyBinds;
+
+
+}
